56f342989d2561c0da75a4ce83b65afd6c10a49d
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-01-12 14:10:25 +0300

    TasksCell Added

--- TodoApp/Controller/TasksViewController.swift
+++ TodoApp/Controller/TasksViewController.swift
@@ -22,6 +22,13 @@ class TasksViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         return collectionView
     }()
+    private let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Hi Ahmet 👋🏻"
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textColor = .white
+        return label
+    }()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,23 +50,29 @@ extension TasksViewController{
 extension TasksViewController{
     private func style(){
         backgroundGradientColor()
+        self.navigationController?.navigationBar.isHidden = true
         newTaskButton.translatesAutoresizingMaskIntoConstraints = false
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(TasksCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.backgroundColor = .lightGray
+        collectionView.backgroundColor = .clear
+        nameLabel.translatesAutoresizingMaskIntoConstraints =  false
     }
     private func layout(){
         view.addSubview(collectionView)
         view.addSubview(newTaskButton)
+        view.addSubview(nameLabel)
         NSLayoutConstraint.activate([
             view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: newTaskButton.bottomAnchor, constant: 30),
             view.trailingAnchor.constraint(equalTo: newTaskButton.trailingAnchor, constant: 4),
             newTaskButton.heightAnchor.constraint(equalToConstant: 60),
             newTaskButton.widthAnchor.constraint(equalToConstant: 60),
             
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            
+            collectionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
             view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 14)
@@ -72,8 +85,16 @@ extension TasksViewController: UICollectionViewDelegate ,UICollectionViewDataSou
         return 5
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TasksCell
         return cell
+    }
+}
+extension TasksViewController: UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width * 0.9, height: 50)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 10, height: 10)
     }
 }
---
+++ TodoApp/View/TasksCell.swift
@@ -0,0 +1,19 @@
+//
+//  TasksCell.swift
+//  TodoApp
+//
+//  Created by hakkı can şengönül on 12.01.2023.
+//
+
+import UIKit
+
+class TasksCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .brown
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
