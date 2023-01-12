741d99ffcfd4622250aa14aa554a2b02cb173b0e
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-01-12 13:38:54 +0300

    TasksCollectionView Added

--- TodoApp/Controller/TasksViewController.swift
+++ TodoApp/Controller/TasksViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+private let reuseIdentifier = "TasksCell"
 class TasksViewController: UIViewController {
     // MARK: - Properties
     private lazy var newTaskButton: UIButton = {
@@ -18,6 +18,10 @@ class TasksViewController: UIViewController {
         button.addTarget(self, action: #selector(handleNewTaskButton), for: .touchUpInside)
         return button
     }()
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collectionView
+    }()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +29,14 @@ class TasksViewController: UIViewController {
         layout()
     }
 }
- // MARK: - Selector
+// MARK: - Selector
 extension TasksViewController{
-   @objc private func handleNewTaskButton(_ sender: UIButton){
+    @objc private func handleNewTaskButton(_ sender: UIButton){
         let controller = NewTaskViewController()
-       if let sheet = controller.sheetPresentationController{
-           sheet.detents = [.medium()]
-       }
-       self.present(controller, animated: true)
+        if let sheet = controller.sheetPresentationController{
+            sheet.detents = [.medium()]
+        }
+        self.present(controller, animated: true)
     }
 }
 // MARK: - Helpers
@@ -40,14 +44,36 @@ extension TasksViewController{
     private func style(){
         backgroundGradientColor()
         newTaskButton.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .lightGray
     }
     private func layout(){
+        view.addSubview(collectionView)
         view.addSubview(newTaskButton)
         NSLayoutConstraint.activate([
             view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: newTaskButton.bottomAnchor, constant: 30),
             view.trailingAnchor.constraint(equalTo: newTaskButton.trailingAnchor, constant: 4),
             newTaskButton.heightAnchor.constraint(equalToConstant: 60),
-            newTaskButton.widthAnchor.constraint(equalToConstant: 60)
+            newTaskButton.widthAnchor.constraint(equalToConstant: 60),
+            
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 14)
         ])
+    }
+}
+// MARK: - UICollectionViewDelegate ,UICollectionViewDataSource
+extension TasksViewController: UICollectionViewDelegate ,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
