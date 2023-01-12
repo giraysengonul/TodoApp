0504324b30515856fd8ddd1bf07ba4cb2310ad05
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-01-12 15:16:28 +0300

    Creation of task cell content

--- TodoApp/View/TasksCell.swift
+++ TodoApp/View/TasksCell.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class TasksCell: UICollectionViewCell {
+    // MARK: - Properties
+    private lazy var circleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "circle"), for: .normal)
+        button.tintColor = .mainColor
+        button.addTarget(self, action: #selector(handleCircleButton), for: .touchUpInside)
+        return button
+    }()
+    private let taskLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.text = "Meyve Al."
+        return label
+    }()
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .brown
+        style()
+        layout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+// MARK: - Selector
+extension TasksCell{
+    @objc private func handleCircleButton(_ sender: UIButton){
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            self.circleButton.alpha = 0
+        } completion: { _ in
+            UIView.animate(withDuration: 0.5, delay: 0) {
+                self.circleButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+                self.circleButton.alpha = 1
+            }
+        }
+        
+    }
+}
+// MARK: - Helpers
+extension TasksCell{
+    private func style(){
+        backgroundColor = .white
+        layer.cornerRadius = 5
+        layer.shadowColor = UIColor.black.withAlphaComponent(0.6).cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 3)
+        layer.shadowOpacity = 5
+        circleButton.translatesAutoresizingMaskIntoConstraints = false
+        taskLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    private func layout(){
+        addSubview(circleButton)
+        addSubview(taskLabel)
+        NSLayoutConstraint.activate([
+            circleButton.heightAnchor.constraint(equalToConstant: 50),
+            circleButton.widthAnchor.constraint(equalToConstant: 50),
+            circleButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            circleButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            
+            taskLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            taskLabel.leadingAnchor.constraint(equalTo: circleButton.trailingAnchor, constant: 8),
+            trailingAnchor.constraint(equalTo: taskLabel.trailingAnchor,constant: 8),
+            bottomAnchor.constraint(equalTo: taskLabel.bottomAnchor, constant: 8)
+        ])
     }
 }
