c68ec28b2202c6974ec76cf0553953ac92f1687e
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-01-13 18:55:49 +0300

    Added collectionview to PastTaskViewController

--- TodoApp/Controller/PastTaskViewController.swift
+++ TodoApp/Controller/PastTaskViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
-
+private let reuseIdentifier = "PastTaskCell"
 class PastTaskViewController: UIViewController {
-     // MARK: - Properties
-     // MARK: - Lifecycle
+    // MARK: - Properties
+    private let collectionView:UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collectionView
+    }()
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        style()
+        layout()
+    }
+}
+// MARK: - Helpers
+extension PastTaskViewController{
+    private func style(){
+        backgroundGradientColor()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.backgroundColor = .clear
+    }
+    private func layout(){
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 14)
+        ])
+    }
+}
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension PastTaskViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .systemBlue
+        return cell
     }
 }
